bf2061d9e3945f39e4590cedfee6dd67b2d9ca99
Vishal Kushwaha <vshal6k@gmail.com>
2026-02-17 19:48:30 +0530

    Practised transient keyword

--- src/practice/A.java
+++ src/practice/A.java
@@ -1,6 +1,17 @@
 package practice;
 
-public sealed class A permits B{
+import java.io.Serializable;
+
+public sealed class A implements Serializable permits B {
+
+    public int id;
+    public transient String name;
+
+    public A(int id, String name){
+        this.id = id;
+        this.name = name;
+    }
+
     private static NestedStaticA nestedStaticA;
 
     public static void createNestedClass(){
--- src/practice/SeralizationPractice.java
+++ src/practice/SeralizationPractice.java
@@ -9,29 +9,59 @@ import java.util.List;
 public class SeralizationPractice {
 
     public static void main() {
-        List<Integer> list;
+        // List<Integer> list;
 
-        try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("/Users/vishalkushwaha/Documents/Java SE 21/java-se-21/src/practice/files/serializationFile.txt"))) {
+        // try (ObjectOutputStream out = new ObjectOutputStream(new
+        // FileOutputStream("/Users/vishalkushwaha/Documents/Java SE
+        // 21/java-se-21/src/practice/files/serializationFile.txt"))) {
 
-            list = List.of(Integer.valueOf(1), Integer.valueOf(2), Integer.valueOf(3));
-            out.writeObject(list);
-            out.flush();
+        // list = List.of(Integer.valueOf(1), Integer.valueOf(2), Integer.valueOf(3));
+        // out.writeObject(list);
+        // out.flush();
+
+        // } catch (Exception e) {
+        // e.printStackTrace();
+        // }
+
+        // list = null;
+
+        // try (ObjectInputStream in = new ObjectInputStream(new
+        // FileInputStream("/Users/vishalkushwaha/Documents/Java SE
+        // 21/java-se-21/src/practice/files/serializationFile.txt"))) {
+
+        // Object o = in.readObject();
+        // if(o instanceof List){
+        // list = (List<Integer>) o;
+        // list.stream().forEach(x -> System.out.println(x));
+        // }
+        // } catch (Exception e) {
+        // e.printStackTrace();
+        // }
+
+        // A a;
+
+        // try (ObjectOutputStream out = new ObjectOutputStream(
+        //         new FileOutputStream("src/practice/files/serializationFile.txt"))) {
             
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        //     a = new A(1, "Vishal");
+        //     out.writeObject(a);
+        //     out.flush();
 
-        list = null;
+        // } catch (Exception e) {
+        //     e.printStackTrace();
+        // }
 
-        try (ObjectInputStream in = new ObjectInputStream(new FileInputStream("/Users/vishalkushwaha/Documents/Java SE 21/java-se-21/src/practice/files/serializationFile.txt"))) {
+        // a = null;
 
-            Object o = in.readObject();
-            if(o instanceof List){
-                list = (List<Integer>) o;
-                list.stream().forEach(x -> System.out.println(x));
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        // try (ObjectInputStream in = new ObjectInputStream(
+        //         new FileInputStream("src/practice/files/serializationFile.txt"))) {
+            
+        // a = (A) in.readObject();
+        // System.out.println(a.id + " " + a.name);
+
+        // } catch (Exception e) {
+        //     e.printStackTrace();
+        // }
+
     }
 }
