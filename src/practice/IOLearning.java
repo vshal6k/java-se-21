48fdeaf422732dd38be8850b93440869dcaca908
Vishal Kushwaha <vshal6k@gmail.com>
2026-02-17 08:31:13 +0530

    Learnt about scanner and console classes.

--- src/practice/IOLearning.java
+++ src/practice/IOLearning.java
@@ -2,6 +2,7 @@ package practice;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.Console;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -12,6 +13,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
+import java.io.PrintStream;
 import java.io.PrintWriter;
 import java.io.Reader;
 import java.io.Writer;
@@ -19,64 +21,84 @@ import java.net.URI;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class IOLearning {
 
-    public static void main(String[] args) {
-        try (
+    public static void main() {
+        // try (
 
-                InputStream in = new FileInputStream(
-                        "/Users/vishalkushwaha/Documents/Java SE 21/java-se-21/src/practice/files/input.txt");
-                OutputStream out = new FileOutputStream(
-                        "/Users/vishalkushwaha/Documents/Java SE 21/java-se-21/src/practice/files/output.txt");
-                Reader inReader = new FileReader(
-                        "/Users/vishalkushwaha/Documents/Java SE 21/java-se-21/src/practice/files/input.txt");
-                Writer outWriter = new FileWriter(
-                        "/Users/vishalkushwaha/Documents/Java SE 21/java-se-21/src/practice/files/output.txt");
-                BufferedReader buffReader = new BufferedReader(new InputStreamReader(in));
+        //         InputStream in = new FileInputStream(
+        //                 "/Users/vishalkushwaha/Documents/Java SE 21/java-se-21/src/practice/files/input.txt");
+        //         OutputStream out = new FileOutputStream(
+        //                 "/Users/vishalkushwaha/Documents/Java SE 21/java-se-21/src/practice/files/output.txt");
+        //         Reader inReader = new FileReader(
+        //                 "/Users/vishalkushwaha/Documents/Java SE 21/java-se-21/src/practice/files/input.txt");
+        //         Writer outWriter = new FileWriter(
+        //                 "/Users/vishalkushwaha/Documents/Java SE 21/java-se-21/src/practice/files/output.txt");
+        //         BufferedReader buffReader = new BufferedReader(new InputStreamReader(in));
 
-                PrintWriter printWriter = new PrintWriter(new OutputStreamWriter(out));
+        //         PrintWriter printWriter = new PrintWriter(new OutputStreamWriter(out));
 
-        ) {
-            // int length = 0;
-            // byte[] buffer = new byte[1024];
-            // length = in.read(buffer);
-            // while (length != -1) {
-            // for (int i = 0; i < length; i++) {
-            // System.out.println(buffer[i]);
-            // }
-            // out.write(buffer, 0, length);
-            // length = in.read(buffer);
-            // }
-            // in.transferTo(out);
-            // byte[] indianRupee = {(byte)226, (byte)130, (byte)185};
-            // out.write(indianRupee);
-            // byte[] inputFileBytes =
-            // Files.readAllBytes(Path.of("/Users/vishalkushwaha/Documents/Java SE
-            // 21/java-se-21/src/practice/files/input.txt"));
-            // for (byte b : inputFileBytes) {
-            // System.out.println(b);
-            // }
-            // Charset utf8 = Charset.forName("utf-8");
-            // int length = 0;
-            // char[] buffer = new char[1024];
-            // length = inReader.read(buffer);
-            // while (length != -1) {
-            // for (int i = 0; i < length; i++) {
-            // System.out.println(buffer[i]);
-            // }
-            // outWriter.write(buffer, 0, length);
-            // length = inReader.read(buffer);
-            // }
-            String line = null;
-            while ((line = buffReader.readLine()) != null) {
-                printWriter.println(line);
-                printWriter.flush();
-            }
+        // ) {
+        //     // int length = 0;
+        //     // byte[] buffer = new byte[1024];
+        //     // length = in.read(buffer);
+        //     // while (length != -1) {
+        //     // for (int i = 0; i < length; i++) {
+        //     // System.out.println(buffer[i]);
+        //     // }
+        //     // out.write(buffer, 0, length);
+        //     // length = in.read(buffer);
+        //     // }
+        //     // in.transferTo(out);
+        //     // byte[] indianRupee = {(byte)226, (byte)130, (byte)185};
+        //     // out.write(indianRupee);
+        //     // byte[] inputFileBytes =
+        //     // Files.readAllBytes(Path.of("/Users/vishalkushwaha/Documents/Java SE
+        //     // 21/java-se-21/src/practice/files/input.txt"));
+        //     // for (byte b : inputFileBytes) {
+        //     // System.out.println(b);
+        //     // }
+        //     // Charset utf8 = Charset.forName("utf-8");
+        //     // int length = 0;
+        //     // char[] buffer = new char[1024];
+        //     // length = inReader.read(buffer);
+        //     // while (length != -1) {
+        //     // for (int i = 0; i < length; i++) {
+        //     // System.out.println(buffer[i]);
+        //     // }
+        //     // outWriter.write(buffer, 0, length);
+        //     // length = inReader.read(buffer);
+        //     // }
+        //     String line = null;
+        //     while ((line = buffReader.readLine()) != null) {
+        //         printWriter.println(line);
+        //         printWriter.flush();
+        //     }
 
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        // } catch (IOException e) {
+        //     e.printStackTrace();
+        // }
+
+        // try (Scanner scanner = new Scanner(System.in)) {
+        //     System.out.println("Hello! Please enter your name and press enter.");
+        //     String name = scanner.nextLine();
+        //     System.out.println("Hello " + name);
+        // } catch (Exception e) {
+        //     e.printStackTrace();
+        // }
+
+        Console console = System.console();
+        // System.out.println("Enter your username");
+        // String userName = console.readLine();
+        // System.out.println("Enter your password");
+        // char[] password = console.readPassword();
+        // String passwordString = Arrays.toString(password);
+        // System.out.println(userName + " " + passwordString);
+        console.writer().println("Hello World!!");
+        console.writer().flush();
+
     }
-
 }
